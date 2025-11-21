1a26232171006331d042cfccf6c00e64c4e41a26
Howard <hw@yuanhau.com>
2025-11-22 00:37:44 +0800

    Changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,5 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name) // {} is %s %f %d etc.. in c++
-}
-
-#[tauri::command]
 fn test_command(pass: &str) -> String {
     // no case on rust
     format!("hello test command! {}", pass) // no ; in rust ig (nvm its not)
@@ -13,8 +8,9 @@ fn test_command(pass: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, test_command])
+        .invoke_handler(tauri::generate_handler![test_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Nav from "./nav";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Nav />
     <App />
   </React.StrictMode>,
 );
